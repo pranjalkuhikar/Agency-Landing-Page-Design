870dac084acba202cb94343571e68936cdfdb15b
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-04-10 13:31:55 +0530

    Refactor Button component to accept text prop and update Section1 to use the new Button implementation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ text }) => {
+  return (
+    <div className="cursor-pointer px-7 py-3 bg-[#4F4AD7] text-white rounded-xl active:scale-95">
+      {text}
+    </div>
+  );
 };
 
 export default Button;
--- src/sections/section1/section1.jsx
+++ src/sections/section1/section1.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Button from "../../components/Button";
 
 const Section1 = () => {
-  return <div>section1</div>;
+  return (
+    <div className="px-40 py-10 flex items-center justify-between">
+      <div className="text-2xl font-semibold tracking-tighter">🅿️Pranjal</div>
+      <div className="flex items-center justify-between gap-10 text-[#00012A]">
+        <div className="cursor-pointer">About Us</div>
+        <div className="cursor-pointer">Services</div>
+        <div className="cursor-pointer">Team</div>
+        <div className="cursor-pointer">Testimonial</div>
+      </div>
+      <Button text={"Contact Us"} />
+    </div>
+  );
 };
 
 export default Section1;
